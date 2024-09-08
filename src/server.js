3f93f11be66d1141f9843092b9665a439b1f5214
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2024-09-08 16:03:57 +0200

    organize contacts routers

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import contactsRouter from './routers/contacts.js';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -21,33 +21,7 @@ export const setupServer = () => {
         ),
     );
 
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getAllContacts();
-
-        res.json({
-            status: 200,
-            message: 'Successfully found contacts!',
-            data: contacts,
-        });
-    });
-
-    app.get('/contacts/:contactId', async (req, res) => {
-        const { contactId } = req.params;
-        const contact = await getContactById(contactId);
-
-        if (!contact) {
-            res.status(404).json({
-                message: 'Contact not found',
-            });
-            return;
-        }
-
-        res.json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-    });
+    app.use('/contacts', contactsRouter);
 
     app.use('*', (req, res, next) => {
         res.status(404).json({
